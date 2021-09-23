eecb195a58e865912706ac74e1a3cf6861ca7c24
耿洪杰 <jie.geng@jitstack.com>
2021-09-23 11:04:23 +0800

    add code

---
+++ log_test.go
@@ -0,0 +1,17 @@
+package glog
+
+import "testing"
+
+func Test_go_init(t *testing.T) {
+	config := &Config{
+		LogSaveName: "test.log",
+		LogSavePath: "./logs",
+	}
+	log := Init(config)
+	log.Info("Test log init success")
+	log.Infof("%s 载人登月成功", "中国")
+	log.Warn("Test log init success")
+	log.Warnf("%s 载人登月成功", "中国")
+	log.Debug("Test log init success")
+	log.Debugf("%s 载人登月成功", "中国")
+}
